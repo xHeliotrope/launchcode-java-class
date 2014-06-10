e6f24f68d6338046cb5b80e9dbad863c2456dd38
Ryan Moore <rmoore8869@gmail.com>
2014-06-10 08:44:52 -0500

    changes made

--- Mario1.java
+++ Mario1.java
@@ -1,55 +1,45 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+
 import static java.lang.Integer.parseInt;
-
-
 
 public class Mario1 {
 
-    public static int inputReader(){
-        int inputInt = -1;
-        String preInt;
+    public static int inputReader() {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        boolean a = true;
-        while(a==true){
+        while (true) {
             System.out.println("Please enter an integer between 0 and "
                     + "23(inclusive).");
-            try{
-            preInt = br.readLine();
-            inputInt = parseInt(preInt);
+            try {
+                int inputInt = parseInt(br.readLine());
+                if (inputInt >= 0 && inputInt <= 23) {
+                    return inputInt;
                 }
-            catch(NumberFormatException | IOException e){
-                System.out.println("Errrr...Does not compute.");
-                }
-            if(inputInt>=0 && inputInt<=23){
-                a=false;
-                }
+            } catch (NumberFormatException | IOException e) {
+                System.out.println("Error: " + e.getMessage());
             }
-    return inputInt;
-    }
-    
-    public static void stepBuilder(int input){
-       for(int i=0; i<=input; i++){
-           int j = i;
-           int k = i;
-           String sb = new String();
-           while(k>0){
-               sb = sb + "#";
-               k--;
-               }
-           while(input-j!=0){
-               sb = " " + sb;
-               j++;
-               }
-           System.out.println(sb);
-           }
-           }
-    
-    public static void main(String[] args){
-        stepBuilder(inputReader());
         }
-}
+    }
 
-            
-            
+    public static void stepBuilder(int input) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i <= input; i++) {
+            int j = i, k = i;
+            while (k > 0) {
+                sb.append("#");
+                k--;
+            }
+            while (input - j != 0) {
+                sb.append(" ");
+                j++;
+            }
+            System.out.println(sb);
+        }
+    }
+
+    public static void main(String[] args) {
+        stepBuilder(inputReader());
+    }
+}
