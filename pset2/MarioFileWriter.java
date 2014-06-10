d6460f7c8bff0dd2bc300bb155884c2f7b7304a4
Ryan Moore <rmoore8869@gmail.com>
2014-06-10 08:54:01 -0500

    changes made

---
+++ pset2/MarioFileWriter.java
@@ -0,0 +1,6 @@
+
+package pset2;
+
+public interface MarioFileWriter{
+    public void outputTriangle(String pyramid);
+}
---
+++ pset2/marioOutputs/ErrorOutput.java
@@ -0,0 +1,14 @@
+
+package pset2.marioOutputs;
+
+import pset2.MarioFileWriter;
+
+
+public class ErrorOutput implements MarioFileWriter{
+    
+    @Override
+    public void outputTriangle(String pyramid){
+        System.out.println("Well, something went wrong with the "
+                + "MarioFileWriter.");
+        }
+        }
